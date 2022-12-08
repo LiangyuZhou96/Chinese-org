15c7530191720e5fb9b6cc0083505d45b4e7a06e
LiangyuZhou96 <liangyuzhou96@gmail.com>
2022-12-08 12:21:46 +0000

    hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react'
 import Hero1 from "../assets/hero1.jpg";
 const Hero = () => {
     const [url, setUrl] = useState(Hero1);
-    
+
     useEffect(() => {
         setUrl(Hero1);
     }, [url]);
-    
-      
+
+
     return (
         <div >
-            <div className='z-0 mt-[-96px] w-full h-screen text-white bg-center bg-cover bg-no-repeat bg-[#00000099] bg-blend-darken' style={{backgroundImage: `url(${url.src})`}}>
-                <div className='  mx-auto text-center flex flex-col justify-center'>
-
+            <div className='z-0 mt-[-96px] w-full h-screen bg-center bg-cover bg-[#00000099] bg-blend-darken' style={{ backgroundImage: `url(${url.src})` }}>
+                <div className='mx-auto w-10/12 h-full flex flex-col justify-center '>
+                    <div className='py-20'></div>
+                    <p className='font-bold text-6xl text-white py-4'>Bring Together</p>
+                    <p className='font-bold text-7xl text-[#f26c4f] py-4'>Be Proud of Chinese Art and </p>
+                    <p className='font-bold text-7xl text-[#f26c4f] pb-4'>Culture</p>
+                    <p className='font-bold text-xl text-white py-4'>The 1st NFT Marketplace that we could participate, promote and govern culture together for a NATION!</p>
+                    <p className=' text-md text-white py-4'>A SocialFi-driven NFTs Creating and Trading Ecosystem for Chinese Culture, Mining the Greatest Value of World-wide Chinese Collection, Together!</p>
                 </div>
             </div>
         </div>
